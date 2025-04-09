065e1eda0c153d46d025461c87cf79683c868db4
Arián Villalba Tapia <avillalbat0907@gmail.com>
2025-04-09 01:34:35 -0600

    Update run_code.py

--- run_code.py
+++ run_code.py
@@ -1,21 +1,51 @@
+
 from data_loader import *
-from CNNN1D import * 
-from metrics import *
+from CNN1D import * 
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, f1_score, ConfusionMatrixDisplay
+import numpy as np
+
 
 if __name__ == "__main__":
   x_train,y_train, x_val, y_val = data_process()
+  x_train_split = [x_train[:, i, :].reshape(-1, 136, 1) for i in range(12)]
+  x_val_split = [x_val[:, i, :].reshape(-1, 136, 1) for i in range(12)]
   model = create_cnn_model(x_train)
-  model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(), metrics=["accuracy"])
+  #model.compile(loss="binary_crossentropy", optimizer=tf.keras.optimizers.Adam(), metrics=["accuracy"])
+  model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+  y_train_onehot = tf.keras.utils.to_categorical(y_train, num_classes=2)
+  y_val_onehot = tf.keras.utils.to_categorical(y_val, num_classes=2)
 
-  epochs = 5
+  epochs = 1
 
-  cnn_model_history = model.fit(x_train, y_train, epochs = epochs, batch_size=10, validation_data=(x_val, y_val))
-  plot_result("loss", cnn_model_history)
-  plot_result("accuracy", cnn_model_history)
-
+  cnn_model_history = model.fit(x_train_split, y_train_onehot, epochs = epochs, batch_size=32, validation_data=(x_val_split, y_val_onehot))
   
-  y_pred = model.predict([ x_test_Wb, x_test_r, x_test_g, x_test_b])
-  pred_labels = np.argmax(y_pred, axis = 1)
-  confusion_matrix_metric(y_val, pred_labels)
+  y_pred_prob = model.predict(x_val_split)
 
 
+  y_pred = np.argmax(y_pred_prob, axis=1)
+  cm = confusion_matrix(np.argmax(y_val_onehot, axis=1), y_pred)
+
+    # Crear la figura de la matriz de confusión
+  fig, ax = plt.subplots(figsize=(6, 5))
+  cax = ax.matshow(cm, cmap='Blues')
+    
+    # Añadir los valores en cada celda
+  for (i, j), value in np.ndenumerate(cm):
+     ax.text(j, i, str(value), ha='center', va='center', color='black')
+    
+    # Añadir etiquetas y título
+  ax.set_xlabel('Predicción')
+  ax.set_ylabel('Real')
+  ax.set_title('Matriz de Confusión')
+    
+    # Añadir las etiquetas de los ejes
+  ax.set_xticks([0, 1])
+  ax.set_yticks([0, 1])
+  ax.set_xticklabels(['Clase 0', 'Clase 1'])
+  ax.set_yticklabels(['Clase 0', 'Clase 1'])
+    
+    # Mostrar la matriz
+  plt.show()
