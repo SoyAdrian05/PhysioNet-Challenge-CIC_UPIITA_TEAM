ad551206aea688c178d8927a5a3c0cc375a45be9
Arián Villalba Tapia <avillalbat0907@gmail.com>
2025-08-26 23:39:42 -0600

    Update team_code.py

--- team_code.py
+++ team_code.py
@@ -12,6 +12,8 @@
 
 import joblib
 import numpy as np
+from scipy.signal import butter, filtfilt
+import pywt
 import os
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import sys
@@ -36,6 +38,100 @@ else:
 # Train your models. This function is *required*. You should edit this function to add your code, but do *not* change the arguments
 # of this function. If you do not train one of the models, then you can return None for the model.
 # Create the tensor
+
+def butter_bandpass(lowcut, highcut, fs, order=3):
+    nyq = 0.5 * fs  
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=3):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = filtfilt(b, a, data)
+    return y
+
+def filtro_señal_3d(tensor_3d):
+
+    n_samples, signal_length, n_channels = tensor_3d.shape
+    tensor_filtrado = np.zeros_like(tensor_3d)
+    
+    for sample in range(n_samples):
+        for channel in range(n_channels):
+            # Extraer la señal de un canal específico de una muestra específica
+            signal = tensor_3d[sample, :, channel]
+            # Aplicar filtro
+            tensor_filtrado[sample, :, channel] = butter_bandpass_filter(
+                signal, 0.5, 150, 400, order=4
+            )
+    
+    return tensor_filtrado
+
+def wavelet_signals(tensor):
+   n,m,chanels = tensor.shape
+   matrix_detail1 = []
+   matrix_detail2 = []
+   matrix_detail3 = []
+   
+   for i in range(n):
+       signal0 = apply_wavelet_transform(tensor[i,:,0])
+       signal1 = apply_wavelet_transform(tensor[i,:,1])
+       signal2 = apply_wavelet_transform(tensor[i,:,2])
+       signal3 = apply_wavelet_transform(tensor[i,:,3])
+       signal4 = apply_wavelet_transform(tensor[i,:,4])
+       signal5 = apply_wavelet_transform(tensor[i,:,5])
+       signal6 = apply_wavelet_transform(tensor[i,:,6])
+       signal7 = apply_wavelet_transform(tensor[i,:,7])
+       signal8 = apply_wavelet_transform(tensor[i,:,8])
+       signal9 = apply_wavelet_transform(tensor[i,:,9])
+       signal10 = apply_wavelet_transform(tensor[i,:,10])
+       signal11 = apply_wavelet_transform(tensor[i,:,11])
+       
+       matrix_detail1.append([signal0[3],signal1[3],signal2[3],
+                              signal3[3],signal4[3],signal5[3],
+                              signal6[3],signal7[3],signal8[3],
+                              signal9[3],signal10[3],signal11[3]]
+                              )
+       matrix_detail2.append([signal0[2],signal1[2],signal2[2],
+                              signal3[2],signal4[2],signal5[2],
+                              signal6[2],signal7[2],signal8[2],
+                              signal9[2],signal10[2],signal11[2]]
+                              )
+       matrix_detail3.append([signal0[1],signal1[1],signal2[1],
+                              signal3[1],signal4[1],signal5[1],
+                              signal6[1],signal7[1],signal8[1],
+                              signal9[1],signal10[1],signal11[1]]
+                              )
+   return np.array(matrix_detail1),np.array(matrix_detail2), np.array(matrix_detail3)
+
+
+# Wavelets discretas
+def apply_wavelet_transform(signal, wavelet='db3', levels=3):
+    coeffs = pywt.wavedec(signal, wavelet, level=levels)
+    return coeffs
+
+def pad_signal_tensor(tensor, target_length=4096):
+    n, m, channels = tensor.shape
+    
+    if m >= target_length:
+        if m > target_length:
+            print(f"")
+        return tensor
+    
+    padding_needed = target_length - m
+    
+    if isinstance(tensor, np.ndarray):
+        padding_zeros = np.zeros((n, padding_needed, channels), dtype=tensor.dtype)
+        padded_tensor = np.concatenate([tensor, padding_zeros], axis=1)
+    else:
+        raise TypeError("El tensor debe ser un torch.Tensor o numpy.ndarray")
+    
+    print(f"Padding aplicado: {m} -> {target_length} (añadidos {padding_needed} ceros)")
+    
+    return padded_tensor
+
+
+
 
 def log_memory(step=""):
     mem = psutil.Process(os.getpid()).memory_info().rss / (1024*1024)
@@ -69,22 +165,7 @@ def create_signal_tensor(signals, signal_info, verbose, target_length):
     return tensor, target_length
 
 ##Crea un dataset para que consuma menos memoria
-def build_dataset(signal_tensor, labels, batch_size=8, shuffle=True):
-    def gen():
-        for i in range(len(labels)):
-            yield tuple(signal_tensor[i, :, j:j+1] for j in range(signal_tensor.shape[2])), labels[i]
-
-    dataset = tf.data.Dataset.from_generator(
-        gen,
-        output_signature=(
-            tuple(tf.TensorSpec(shape=(signal_tensor.shape[1], 1), dtype=tf.float32) for _ in range(signal_tensor.shape[2])),
-            tf.TensorSpec(shape=(), dtype=tf.int32)
-        )
-    )
-    if shuffle:
-        dataset = dataset.shuffle(buffer_size=len(labels))
-    dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
-    return dataset
+ 
 
 # Train your model.
 def train_model(data_folder, model_folder, verbose):
@@ -132,6 +213,11 @@ def train_model(data_folder, model_folder, verbose):
     labels = np.asarray(labels, dtype=bool)
     
     signal_tensor, padded_length = create_signal_tensor(all_signals, signal_info, verbose, target_length = False)
+    print(signal_tensor.shape)
+    signal_tensor = pad_signal_tensor(signal_tensor)
+    tensor_butter = filtro_señal_3d(signal_tensor)
+    detail1,detail2,detail3 = wavelet_signals(tensor_butter)
+    input_train = [detail1,detail2,detail3]
 
     selected_channels = ['I', 'II', 'III', 'V1', 'V2', 'V3']
     # Train the models on the signals.
@@ -147,8 +233,8 @@ def train_model(data_folder, model_folder, verbose):
     # Crear lista de señales solo con los canales seleccionados
     signal_list = [signal_tensor[:, :, i:i+1] for i in selected_indices]
     
-    dataset = build_dataset(signal_tensor, labels, batch_size=8)
-    model = create_cnn_model(signal_tensor)
+    # dataset = build_dataset(signal_tensor, labels, batch_size=8)
+    model = cnn_model2()
     
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
                   loss="binary_crossentropy",
@@ -167,7 +253,7 @@ def train_model(data_folder, model_folder, verbose):
     epochs = 40
 
     log_memory("Before training")
-    cnn_model_history = model.fit(dataset, epochs = epochs)
+    cnn_model_history = model.fit([detail1,detail2,detail3],labels, epochs = epochs)
 
     log_memory("After Training")
     # Create a folder for the model if it does not already exist.
@@ -234,15 +320,19 @@ def run_model(data_folder, model, verbose):
         print('Training the model on the signal data...')
         print(f'Signal tensor shape: {signal_tensor.shape}')
         print(f'Padded length: {padded_length}')
-        
     
-    signal_list = [signal_tensor[:, :, i:i+1] for i in range(signal_tensor.shape[2])]
+    signal_tensor = pad_signal_tensor(signal_tensor)
+    signal_tensor_raw = signal_tensor
+    tensor_butter = filtro_señal_3d(signal_tensor_raw)
+    detail1,detail2,detail3 = wavelet_signals(tensor_butter)
+    input_train = [detail1,detail2,detail3]
+    # signal_list = [signal_tensor[:, :, i:i+1] for i in range(signal_tensor.shape[2])]
     
     if verbose:
         print(f'Signal list length: {len(signal_list)}')
         print(f'Each signal shape: {signal_list[0].shape}')
     
-    probability_output = model.predict(signal_list, verbose=1)      
+    probability_output = model.predict(input_train, verbose=1)      
     
     binary_outputs = (probability_output >= 0.5).astype(int)
     return binary_outputs, probability_output
