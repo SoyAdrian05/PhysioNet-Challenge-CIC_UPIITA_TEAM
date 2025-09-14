a0760e77adcae9490918b86c07ae81723df144d9
Adrian <94141362+SoyAdrian05@users.noreply.github.com>
2025-09-14 10:14:13 -0600

    Update team_code.py

--- team_code.py
+++ team_code.py
@@ -229,7 +229,8 @@ def train_model(data_folder, model_folder, verbose):
     batch_size = 16
     canales = [0, 4, 6, 8]
     #canales = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    
+
+    print('num_canales: ', canales)
     signal_files = os.listdir(signal_folder)
     num_files = len(signal_files)
     
